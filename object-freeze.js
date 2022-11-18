5b180ad1b513743550ea6db19473e77deb707d8c
jeonhyoungmin <jhmin7509@gmail.com>
2022-11-18 10:50:11 +0900

    python tuple, js freeze

---
+++ object-freeze.js
@@ -0,0 +1,10 @@
+// object-freeze.js
+const pokeMutable = ["피카츄", "라이츄", "파이리", "꼬부기", "버터풀"];
+pokeMutable.push('두트리오');
+
+const pokeImmutable = ['메타몽', '리자몽', '왕눈해', '토게피', '고오스'];
+Object.freeze(pokeImmutable)
+// pokeImmutable.push('두트리오')
+
+console.log(pokeMutable)
+console.log(pokeImmutable)
